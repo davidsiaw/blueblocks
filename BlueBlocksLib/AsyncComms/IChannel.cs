ea147cb253a9f5a8b363d45bf870937af81ef6ae
David Siaw <davidsiaw@gmail.com>
2010-11-15 09:05:27 +1300

    First commit\!

---
+++ BlueBlocksLib/AsyncComms/IChannel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueBlocksLib.AsyncComms
+{
+    public interface IChannel<T>
+    {
+        void Send(T v);
+        T Receive();
+    }
+}
---
+++ BlueBlocksLib/BaseClasses/DelegateTypes.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueBlocksLib
+{
+    public delegate void Action();
+    public delegate TResult Func<T, TResult>(T arg1);
+}
